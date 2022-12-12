87ecbc178413761ea7c64bd90b1e32ce43a808a2
IrmaDong <irmadong@irmadongs-mbp.myfiosgateway.com>
2022-12-12 13:14:36 -0500

    basic framework for testing)

---
+++ finetune_framework.py
@@ -0,0 +1,1 @@
+#credit to :https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
---
+++ utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+import torch
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+def get_pred(model, images, device):
+    logits = model(images.to(device))
+    _, pres = logits.max(dim=1)
+    return pres.cpu()
+
+def imshow(img, title):
+    img = torchvision.utils.make_grid(img.cpu().data, normalize=True)
+    npimg = img.numpy()
+    fig = plt.figure(figsize = (5, 15))
+    plt.imshow(np.transpose(npimg,(1,2,0)))
+    plt.title(title)
+    plt.show()
+
+
+def load_dataset(n_examples, loader, batch_size = 100) :
+
+    x_test, y_test = [], []
+    for i, (x, y) in enumerate(loader):
+        x_test.append(x)
+        #print(x.shape)
+        y_test.append(y)
+        if n_examples is not None and batch_size * i >= n_examples:
+            break
+    x_test_tensor = torch.cat(x_test)
+    y_test_tensor = torch.cat(y_test)
+
+    if n_examples is not None:
+        x_test_tensor = x_test_tensor[:n_examples]
+        y_test_tensor = y_test_tensor[:n_examples]
+
+    return x_test_tensor, y_test_tensor
+
+
+def CIFAR10(batch_size=128, test_batch_size=128):
+    transform = transforms.Compose(
+        [transforms.ToTensor()])
+    train_dataset = datasets.CIFAR10('./datasets/CIFAR-10', train=True,
+                                     download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataset = datasets.CIFAR10('./datasets/CIFAR-10', train=False, download=True,
+                                   transform=transform)
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=test_batch_size, shuffle=True)
+
+    return train_dataset, val_dataset, train_loader, val_loader
